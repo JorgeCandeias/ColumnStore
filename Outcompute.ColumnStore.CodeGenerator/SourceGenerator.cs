de49521b6341904d2269702947fe6972313300b8
Jorge Candeias <jorge.candeias@outcompute.com>
2022-05-26 14:30:00 +0100

    Added hack to dedup Orleans assembly-based generated code

--- Outcompute.ColumnStore.CodeGenerator/SourceGenerator.cs
+++ Outcompute.ColumnStore.CodeGenerator/SourceGenerator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Outcompute.ColumnStore.CodeGenerator
@@ -18,14 +19,38 @@ namespace Outcompute.ColumnStore.CodeGenerator
                 return;
             }
 
+            var assemblyName = context.Compilation.AssemblyName ?? "Assembly";
             var libs = LibraryTypes.FromCompilation(context.Compilation);
 
             var result = ColumnStoreSourceDriver.Generate(context.Compilation, receiver.Model, libs);
             var text = result.NormalizeWhitespace().ToFullString();
 
-            context.AddSource($"{context.Compilation.AssemblyName ?? "Assembly"}.ColumnStore.g.cs", text);
+            context.AddSource($"{assemblyName}.ColumnStore.g.cs", text);
 
-            OrleansSerializationSourceDriver.Generate(context.Compilation, new[] { SourceText.From(text) });
+            // hack to de-duplicate the orleans assembly metadata class name
+            var search = $"Metadata_{context.Compilation.AssemblyName!.Replace('.', '_')}";
+            var replace = $"Metadata_ColumnStoreCodeGen_{context.Compilation.AssemblyName!.Replace('.', '_')}";
+            var changes = new List<TextChange>();
+
+            foreach (var item in OrleansSerializationSourceDriver.Generate(context.Compilation, new[] { SourceText.From(text) }))
+            {
+                var source = item.SourceText;
+                foreach (var line in source.Lines)
+                {
+                    var index = line.ToString().IndexOf(search);
+                    if (index >= 0)
+                    {
+                        changes.Add(new TextChange(new TextSpan(line.Start + index, search.Length), replace));
+                    }
+                }
+
+                if (changes.Count > 0)
+                {
+                    source = source.WithChanges(changes);
+                }
+
+                context.AddSource($"{context.Compilation.AssemblyName}.ColumnStore.{item.HintName}", source);
+            }
         }
     }
 }
