9f027c765f7fc37a9522d7a7c1f09e28ba097144
Jorge Candeias <jorge.candeias@outcompute.com>
2022-06-22 22:09:45 +0100

    WIP Encodings

--- Outcompute.ColumnStore.Tests/DefaultEncodingTests.cs
+++ Outcompute.ColumnStore.Tests/DefaultEncodingTests.cs
@@ -9,33 +9,58 @@ public class DefaultEncodingTests
         .AddColumnStore()
         .BuildServiceProvider();
 
-    public class RoundtripData : TheoryData<int, int, IEnumerable<int>>
+    public class RoundtripInt32Data : TheoryData<int, int, IEnumerable<int>>
     {
-        public RoundtripData()
+        public RoundtripInt32Data()
         {
-            // empty value set
+            // empty set
             Add(2, 0, Array.Empty<int>());
 
-            // small value set
+            // small set
             Add(28, 13, new[] { 1, 2, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3, 4 });
 
-            // large value set with min cardinality
+            // large set with min cardinality
             Add(2000004, 1_000_000, Enumerable.Repeat(1, 1_000_000));
 
-            // large value set with sparse cardinality
-            Add(2936004, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => x % 1000));
+            // large set with sparse cardinality
+            Add(2360005, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => x / 10000));
 
-            // large value set with max cardinality
+            // large set with wave cardinality
+            Add(3174404, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => x % 10000));
+
+            // large set with max cardinality
             Add(3991750, 1_000_000, Enumerable.Range(1, 1_000_000));
         }
     }
 
-    [Theory]
-    [ClassData(typeof(RoundtripData))]
-    public void Roundtrips(int expectedLength, int bufferLength, IEnumerable<int> source)
+    public class RoundtripStringData : TheoryData<int, int, IEnumerable<string>>
+    {
+        public RoundtripStringData()
+        {
+            // empty set
+            Add(2, 0, Array.Empty<string>());
+
+            // small set
+            Add(32, 13, new[] { "A", "B", "C", "A", "A", "A", "B", "B", "B", "C", "C", "C", "D" });
+
+            // large set with min cardinality
+            Add(2000009, 1_000_000, Enumerable.Repeat("Value", 1_000_000));
+
+            // large set with sparse cardinality
+            Add(8900006, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => $"Value{x / 10000}"));
+
+            // large set with wave cardinality
+            Add(10889004, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => $"Value{x % 10000}"));
+
+            // large set with max cardinality
+            Add(12888900, 1_000_000, Enumerable.Range(1, 1_000_000).Select(x => $"Value{x}"));
+        }
+    }
+
+    private void RoundtripCore<T>(int expectedLength, int bufferLength, IEnumerable<T> source)
     {
         // materialize the test data
-        using var owner = SpanOwner<int>.Allocate(bufferLength);
+        using var owner = SpanOwner<T>.Allocate(bufferLength);
         var data = owner.Span;
         var added = 0;
         foreach (var item in source)
@@ -45,7 +70,7 @@ public class DefaultEncodingTests
         Assert.Equal(added, bufferLength);
 
         // arrange
-        var encoding = _provider.GetRequiredService<DefaultEncoding<int>>();
+        var encoding = _provider.GetRequiredService<DefaultEncoding<T>>();
 
         // act - encode
         using var encoded = encoding.Encode(data);
@@ -58,5 +83,19 @@ public class DefaultEncodingTests
 
         // assert - decoded
         Assert.True(data.SequenceEqual(decoded.Span));
+    }
+
+    [Theory]
+    [ClassData(typeof(RoundtripInt32Data))]
+    public void RoundtripInt32(int expectedLength, int bufferLength, IEnumerable<int> source)
+    {
+        RoundtripCore(expectedLength, bufferLength, source);
+    }
+
+    [Theory]
+    [ClassData(typeof(RoundtripStringData))]
+    public void RoundtripString(int expectedLength, int bufferLength, IEnumerable<string> source)
+    {
+        RoundtripCore(expectedLength, bufferLength, source);
     }
 }
