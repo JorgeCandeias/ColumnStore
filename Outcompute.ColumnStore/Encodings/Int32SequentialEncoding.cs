e53a544f0daf1e4a2953f8b40999044782857627
Jorge Candeias <jorge.candeias@outcompute.com>
2022-07-04 09:27:43 +0100

    Cleanup

--- Outcompute.ColumnStore/Encodings/Int32SequentialEncoding.cs
+++ Outcompute.ColumnStore/Encodings/Int32SequentialEncoding.cs
@@ -10,13 +10,11 @@ internal class Int32SequentialEncoding : SequentialEncoding<int>
 
     protected override void Serialize<TBufferWriter>(ref Writer<TBufferWriter> writer, int value)
     {
-        //writer.Write7BitEncodedInt(value);
         writer.WriteVarUInt32((uint)value);
     }
 
     protected override int Deserialize<TInput>(ref Reader<TInput> reader)
     {
-        //return reader.Read7BitEncodedInt();
         return (int)reader.ReadVarUInt32();
     }
 }
