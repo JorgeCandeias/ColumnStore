bc2db645f69981db2638e05f5572842d4dc5b2c3
Jorge Candeias <jorge.candeias@outcompute.com>
2022-08-01 18:23:56 +0100

    Added AlignedNativeMemoryOwner + Writer

--- Outcompute.ColumnStore/Core/Buffers/AlignedNativeMemoryOwner.cs
+++ Outcompute.ColumnStore/Core/Buffers/AlignedNativeMemoryOwner.cs
@@ -4,9 +4,9 @@ namespace Outcompute.ColumnStore.Core.Buffers;
 
 /// <summary>
 /// An <see cref="IMemoryOwner{T}"/> implementation that allocates native aligned memory.
-/// It is capable of resizing the underlying buffer up and down but not slicing.
+/// It is capable of resizing the underlying buffer up and down but not slicing yet.
 /// </summary>
-internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
+internal sealed class AlignedNativeMemoryOwner<T> : MemoryManager<T>
     where T : unmanaged
 {
     /// <summary>
@@ -20,26 +20,31 @@ internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
     private unsafe byte* _memory;
 
     /// <summary>
-    /// The sliced length of the buffer.
+    /// The length of the buffer.
     /// </summary>
     private int _length;
 
     /// <summary>
     /// The alignment of the buffer;
     /// </summary>
-    private int _alignment;
+    private readonly int _alignment;
 
     /// <summary>
-    /// Used for interlocked disposed checking.
+    /// Used for the interlocked dispose check.
     /// </summary>
     private int _disposed = 0;
 
-    private unsafe AlignedMemoryOwner(byte* memory, int length, int alignment)
+    private unsafe AlignedNativeMemoryOwner(byte* memory, int length, int alignment)
     {
         _memory = memory;
         _length = length;
         _alignment = alignment;
     }
+
+    /// <summary>
+    /// The current length of the buffer.
+    /// </summary>
+    public int Length => _length;
 
     public override unsafe Span<T> GetSpan()
     {
@@ -48,11 +53,10 @@ internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
 
     public override unsafe MemoryHandle Pin(int elementIndex = 0)
     {
-        // validation here accounts for slicing
         Guard.IsGreaterThanOrEqualTo(elementIndex, 0, nameof(elementIndex));
         Guard.IsLessThan(elementIndex, _length, nameof(elementIndex));
 
-        // no need to pin native memory as the gc cannot move it so just return a dummy handle
+        // no need to pin native memory as the gc cannot move it so just return a plain handle
         return new(_memory + elementIndex, default, this);
     }
 
@@ -62,36 +66,26 @@ internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
     }
 
     /// <summary>
-    /// Resizes the underlying memory buffer up or down and/or changes its alignment.
+    /// Resizes the underlying memory buffer up or down.
     /// </summary>
     /// <param name="length">The new length of the buffer.</param>
-    /// <param name="clear">Whether to clear the newly allocated area of the buffer.</param>
-    /// <param name="alignment">The new alignment of the buffer.</param>
-    public unsafe void Resize(int length, bool clear = false, int alignment = DefaultAlignment)
+    /// <param name="clear">Whether to clear the newly allocated portion of the buffer when enlarging it.</param>
+    public unsafe void Resize(int length, bool clear = false)
     {
         Guard.IsGreaterThanOrEqualTo(length, 0, nameof(length));
-        Guard.IsGreaterThan(alignment, 0, nameof(alignment));
 
         // noop if nothing will change
-        if (length == _length && alignment == _alignment)
+        if (length == _length)
         {
             return;
         }
 
-        // compute new byte length
+        // calculate new byte length
         var size = sizeof(T);
         var bytes = length * size;
 
         // resize the native memory block
-        _memory = (byte*)NativeMemory.AlignedRealloc(_memory, (uint)bytes, (uint)alignment);
-
-        // clear the new bytes if request
-        if (clear && length > _length)
-        {
-            var span = new Span<T>(_memory, length);
-
-            span[_length..].Clear();
-        }
+        _memory = (byte*)NativeMemory.AlignedRealloc(_memory, (uint)bytes, (uint)_alignment);
 
         // adjust memory pressure
         var diff = length - _length;
@@ -104,24 +98,32 @@ internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
             GC.RemoveMemoryPressure(-diff);
         }
 
+        // clear the new bytes if requested and applicable
+        if (clear && length > _length)
+        {
+            var span = new Span<T>(_memory, length);
+
+            span[_length..].Clear();
+        }
+
         // done
         _length = length;
-        _alignment = alignment;
     }
 
     protected override unsafe void Dispose(bool disposing)
     {
+        // this guarantees the buffer is only released once
         if (Interlocked.CompareExchange(ref _disposed, 1, 0) != 0)
         {
             return;
         }
 
+        // now it is safe to release the buffer
         NativeMemory.AlignedFree(_memory);
-
         GC.RemoveMemoryPressure(_length);
     }
 
-    public static unsafe AlignedMemoryOwner<T> Allocate(int length, int alignment = 64)
+    public static unsafe AlignedNativeMemoryOwner<T> Allocate(int length, bool clear = false, int alignment = DefaultAlignment)
     {
         Guard.IsGreaterThanOrEqualTo(length, 0, nameof(length));
         Guard.IsGreaterThan(alignment, 0, nameof(alignment));
@@ -136,7 +138,15 @@ internal sealed class AlignedMemoryOwner<T> : MemoryManager<T>
         // adjust memory pressure
         GC.AddMemoryPressure(length);
 
+        // clear the new bytes if requested
+        if (clear)
+        {
+            var span = new Span<T>(memory, length);
+
+            span.Clear();
+        }
+
         // create a new owner for the user to release the buffer
-        return new AlignedMemoryOwner<T>(memory, length, alignment);
+        return new AlignedNativeMemoryOwner<T>(memory, length, alignment);
     }
 }
---
+++ Outcompute.ColumnStore/Core/Buffers/AlignedNativeMemoryOwnerBufferWriter.cs
@@ -0,0 +1,181 @@
+﻿using System.Numerics;
+
+namespace Outcompute.ColumnStore.Core.Buffers;
+
+/// <summary>
+/// A buffer writer over an underlying <see cref="AlignedNativeMemoryOwner{T}"/>.
+/// This writer will resize the underlying buffer as required.
+/// </summary>
+internal sealed class AlignedNativeMemoryOwnerBufferWriter<T> : IBuffer<T>, IDisposable
+    where T : unmanaged
+{
+    /// <summary>
+    /// The default initial capacity of the buffer.
+    /// There is no reasoning as of yet behind this default.
+    /// For ideal performance, the user should select an initial capacity as close as possible to the content length.
+    /// </summary>
+    public const int DefaultInitialCapacity = 1024;
+
+    /// <summary>
+    /// The underlying buffer managed by this writer.
+    /// </summary>
+    private readonly AlignedNativeMemoryOwner<T> _owner;
+
+    /// <summary>
+    /// The next writing position, also doubles as the count of written items.
+    /// </summary>
+    private int _index;
+
+    /// <summary>
+    /// Whether to resize the buffer on overflow.
+    /// </summary>
+    private readonly bool _resize;
+
+    /// <summary>
+    /// Whether to clear new portions of the buffer when auto-resizing.
+    /// Enable this when advancing past written portions of the buffer to avoid acquiring garbage data.
+    /// Otherwise leave disabled to maximize resizing performance.
+    /// </summary>
+    private readonly bool _clear;
+
+    /// <summary>
+    /// Whether to dispose the buffer when this writer instance is disposed.
+    /// </summary>
+    private readonly bool _dispose;
+
+    /// <summary>
+    /// Best effort to avoid disposing multiple times.
+    /// </summary>
+    private bool _disposed;
+
+    /// <summary>
+    /// Creates an instance of <see cref="AlignedNativeMemoryOwnerBufferWriter{T}"/> over an internal <see cref="AlignedNativeMemoryOwner{T}"/> instance.
+    /// </summary>
+    /// <param name="initialLength">The initial capacity of the underlying buffer.</param>
+    /// <param name="alignment">The alignment of the underlying buffer.</param>
+    public AlignedNativeMemoryOwnerBufferWriter(int initialCapacity = DefaultInitialCapacity, bool clear = false, int alignment = AlignedNativeMemoryOwner<T>.DefaultAlignment)
+    {
+        Guard.IsGreaterThanOrEqualTo(initialCapacity, 0, nameof(initialCapacity));
+        Guard.IsGreaterThan(alignment, 0, nameof(alignment));
+
+        _owner = AlignedNativeMemoryOwner<T>.Allocate(initialCapacity, false, alignment);
+        _resize = true;
+        _clear = clear;
+        _dispose = true;
+    }
+
+    /// <summary>
+    /// Creates an instance of <see cref="AlignedNativeMemoryOwnerBufferWriter{T}"/> over the specified <see cref="AlignedNativeMemoryOwner{T}"/> instance.
+    /// </summary>
+    /// <param name="owner">The memory owner that this writer will write to.</param>
+    /// <param name="index">The index at which to start writing.</param>
+    /// <param name="resize">Whether to auto-resize the underlying buffer on overflow.</param>
+    /// <param name="dispose">Whether to dispose the underlying buffer when this writer is disposed.</param>
+    public AlignedNativeMemoryOwnerBufferWriter(AlignedNativeMemoryOwner<T> owner, int index = 0, bool resize = false, bool clear = false, bool dispose = false)
+    {
+        Guard.IsNotNull(owner, nameof(owner));
+        Guard.IsGreaterThanOrEqualTo(index, 0, nameof(index));
+        Guard.IsLessThan(index, owner.Length, nameof(index));
+
+        _owner = owner;
+        _index = index;
+        _resize = resize;
+        _clear = clear;
+        _dispose = dispose;
+    }
+
+    private void AutoResize(int target)
+    {
+        // noop if no resizing needed
+        if (target <= _owner.Length)
+        {
+            return;
+        }
+
+        // resize to the next power of two
+        target = (int)BitOperations.RoundUpToPowerOf2((uint)target);
+
+        // throw if not allowed to resize
+        if (!_resize)
+        {
+            ThrowHelper.ThrowInvalidOperationException($"Cannot auto-resize buffer from {_owner.Length} to {target} because auto-resizing is not enabled");
+        }
+
+        // apply the resize operation
+        _owner.Resize(target, _clear);
+    }
+
+    public void Advance(int count)
+    {
+        _index += count;
+
+        AutoResize(_index);
+    }
+
+    public Memory<T> GetMemory(int sizeHint = 0)
+    {
+        Guard.IsGreaterThanOrEqualTo(sizeHint, 0, nameof(sizeHint));
+
+        // ensure the writing buffer can hold at least one slot
+        AutoResize(_index + Math.Max(sizeHint, 1));
+
+        // return the available writing buffer
+        return _owner.Memory[_index..];
+    }
+
+    public Span<T> GetSpan(int sizeHint = 0)
+    {
+        Guard.IsGreaterThanOrEqualTo(sizeHint, 0, nameof(sizeHint));
+
+        // ensure the writing buffer can hold at least one slot
+        AutoResize(_index + Math.Max(sizeHint, 1));
+
+        // return the available writing buffer
+        return _owner.GetSpan()[_index..];
+    }
+
+    /// <summary>
+    /// Resizes the buffer down to the written portion, releasing the memory from the unwritten portion.
+    /// Call this when you do not want to write to the buffer any further yet do want to hold on to the buffer for a long time.
+    /// </summary>
+    public void Trim()
+    {
+        _owner.Resize(_index, _clear);
+    }
+
+    public void Clear()
+    {
+        _index = 0;
+    }
+
+    public ReadOnlyMemory<T> WrittenMemory => _owner.Memory[.._index];
+
+    public ReadOnlySpan<T> WrittenSpan => _owner.GetSpan()[.._index];
+
+    public int WrittenCount => _index;
+
+    public int Capacity => _owner.Length;
+
+    public int FreeCapacity => _owner.Length - _index;
+
+    public void Dispose()
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (_dispose)
+        {
+            ((IDisposable)_owner).Dispose();
+        }
+
+        GC.SuppressFinalize(this);
+        _disposed = true;
+    }
+
+    ~AlignedNativeMemoryOwnerBufferWriter()
+    {
+        Dispose();
+    }
+}
